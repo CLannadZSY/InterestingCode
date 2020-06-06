9389472df615ca78a08af1a70f59fd55144d5fa6
CLannadZSY <zsymidi@gmail.com>
2020-06-06 16:51:24 +0800

    Update interest_list.py
    粘性之禅

--- interest_list.py
+++ interest_list.py
@@ -375,3 +375,17 @@ for 实现死循环
 """
 for i in iter(int, 1):
     print(i)
+    
+"""
+粘性之禅
+"""
+def product(*args, repeat=1):
+    pools = [tuple(pool) for pool in args] * repeat
+    result = [[]]
+    for pool in pools:
+        result = [x+[y] for x in result for y in pool]
+    for prod in result:
+        yield tuple(prod)
+        
+rtn = product('xyz', '12', repeat=3)
+print(list(rtn))        
